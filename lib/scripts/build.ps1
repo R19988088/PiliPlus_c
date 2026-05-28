9a87d30a73e22fb2784b0865c1b12dbff20edf4c
ddd <ddd@ddddeMacBook-Air.local>
2026-05-28 22:55:57 +0800

    feat: switch theme color sliders to hsb

--- lib/scripts/build.ps1
+++ lib/scripts/build.ps1
@@ -9,11 +9,14 @@ try {
 
     $commitHash = (git rev-parse HEAD).Trim()
 
+    $displayVersionName = $null
+
     $updatedContent = foreach ($line in (Get-Content -Path 'pubspec.yaml' -Encoding UTF8)) {
         if ($line -match '^\s*version:\s*([\d\.]+)') {
             $versionName = $matches[1]
+            $displayVersionName = if ($versionName -eq '0.0.1') { '0.01' } else { $versionName }
             if ($Arg -eq 'android') {
-                $versionName += '-' + $commitHash.Substring(0, 9)
+                $displayVersionName += '-' + $commitHash.Substring(0, 9)
             }
             "version: $versionName+$versionCode"
         }
@@ -25,13 +28,16 @@ try {
     if ($null -eq $versionName) {
         throw 'version not found'
     }
+    if ($null -eq $displayVersionName) {
+        throw 'display version not found'
+    }
 
     $updatedContent | Set-Content -Path 'pubspec.yaml' -Encoding UTF8
 
     $buildTime = [int]([DateTimeOffset]::Now.ToUnixTimeSeconds())
 
     $data = @{
-        'pili.name' = $versionName
+        'pili.name' = $displayVersionName
         'pili.code' = $versionCode
         'pili.hash' = $commitHash
         'pili.time' = $buildTime
@@ -39,7 +45,7 @@ try {
 
     $data | ConvertTo-Json -Compress | Out-File 'pili_release.json' -Encoding UTF8
 
-    Add-Content -Path $env:GITHUB_ENV -Value "version=$versionName+$versionCode"
+    Add-Content -Path $env:GITHUB_ENV -Value "version=$displayVersionName+$versionCode"
 }
 catch {
     Write-Error "Prebuild Error: $($_.Exception.Message)"
